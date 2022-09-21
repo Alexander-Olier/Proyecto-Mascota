58a0bdd563ecab3a384b24f02b699f82c12aa14f
Mateo-olier <aolierguerrero@outlook.com>
2022-09-20 20:53:52 -0500

    user:listo, falta login

---
+++ backend/src/main/java/com/mascota/backend/controller/UserController.java
@@ -0,0 +1,60 @@
+package com.mascota.backend.controller;
+
+import com.mascota.backend.model.User;
+import com.mascota.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/add")
+    public String add(@RequestBody User user) {
+        userService.save(user);
+        return ("new user");
+    }
+
+    @GetMapping("/all")
+    public List<User> getAll() {
+        return userService.getAll();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String delete(@PathVariable Integer id) {
+        userService.delete(id);
+        return "correct delete";
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> get(@PathVariable Integer id) {
+        try {
+            User user = userService.getId(id);
+            return new ResponseEntity<User>(user, HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/update/{id}")
+    public String update(@RequestBody User user, @PathVariable Integer id) {
+        try {
+            User existingUser = userService.getId(id);
+            existingUser = user;
+            existingUser.setId(id);
+            userService.save(existingUser);
+            return "Actualizado";
+        } catch (NoSuchElementException e) {
+            return "ERROR";
+        }
+    }
+
+
+}
---
+++ backend/src/main/java/com/mascota/backend/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.mascota.backend.repository;
+
+import com.mascota.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
