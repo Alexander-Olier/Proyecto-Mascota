7e1f9eacf715b0b7e250e4d71bed6526030e2051
Mateo-olier <aolierguerrero@outlook.com>
2022-11-02 14:59:06 -0500

    Login, register, list: estan listo

--- src/main/java/com/mascota/backend/controller/PostController.java
+++ src/main/java/com/mascota/backend/controller/PostController.java
@@ -4,15 +4,10 @@ import com.mascota.backend.model.Post;
 import com.mascota.backend.model.User;
 import com.mascota.backend.repository.PostRepository;
 import com.mascota.backend.repository.UserRepository;
-import com.mascota.backend.service.PostService;
-import com.mascota.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.NoHandlerFoundException;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import com.mascota.backend.exception.*;
 
 import java.util.List;
 
@@ -24,12 +19,20 @@ public class PostController {
     @Autowired
     private UserRepository userRepository;
 
+    @GetMapping("/home/{id}")
+    public String home(Model model) {
+        List<Post> posts = postRepository.findAll();
+        model.addAttribute("posts", posts);
+        return "home";
+    }
+
     @PostMapping("/add/{id}/posts")
-    public Post savePost(@PathVariable(value = "id") Integer id,  @RequestBody Post postRequest) {
-        Post post = userRepository.findById(id).map(user ->{
+    public Post savePost(@PathVariable(value = "id") Integer id, @RequestBody Post postRequest) {
+        Post post = userRepository.findById(id).map(user -> {
             postRequest.setUser(user);
             return postRepository.save(postRequest);
         }).orElseThrow();
         return postRepository.save(post);
     }
+
 }
--- src/main/java/com/mascota/backend/controller/UserController.java
+++ src/main/java/com/mascota/backend/controller/UserController.java
@@ -1,14 +1,59 @@
 package com.mascota.backend.controller;
 
-
+import com.mascota.backend.model.User;
+import com.mascota.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.List;
+
 @Controller
 @RequestMapping
 public class UserController {
+    @Autowired
+    private UserRepository userRepository;
 
+    @GetMapping("/add")
+    public String create(Model model) {
+        model.addAttribute("user", new User());
+        return "register";
+    }
+
+    @PostMapping("/add")
+    public String add(@ModelAttribute User user, RedirectAttributes redirectAttributes) {
+        userRepository.save(user);
+        redirectAttributes.addFlashAttribute("user", user);
+        return "redirect:/home/" + user.getId();
+    }
+
+    //login
+    @GetMapping("/")
+    public String login(Model model) {
+        model.addAttribute("user", new User());
+        return "login";
+    }
+
+    @PostMapping("/")
+    public String authLogin(@ModelAttribute @RequestBody User userRequest, RedirectAttributes redirectAttributes) {
+        List<User> users = userRepository.findAll();
+        String mail = userRequest.getMail();
+        String password = userRequest.getPassword();
+        for (int x = 0; x < users.size(); x++) {
+            User u = users.get(x);
+            if (u.getMail().equals(mail)) {
+                if (u.getPassword().equals(password)) {
+                    redirectAttributes.addFlashAttribute("user", u);
+                    return "redirect:/home/" + u.getId();
+                }else{
+                    System.out.println("password incorrect");
+                }
+            } else {
+                System.out.println("NotEqual");
+            }
+        }
+        return "error";
+    }
 }
