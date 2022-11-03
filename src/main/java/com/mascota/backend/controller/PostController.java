7393df9cc5d0b72827b86cd0adad8f850a3af1c8
Mateo-olier <aolierguerrero@outlook.com>
2022-11-02 20:37:09 -0500

    bug register post

--- src/main/java/com/mascota/backend/controller/PostController.java
+++ src/main/java/com/mascota/backend/controller/PostController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -22,17 +23,21 @@ public class PostController {
     @GetMapping("/home/{id}")
     public String home(Model model) {
         List<Post> posts = postRepository.findAll();
+        model.addAttribute("postSave", new Post());
         model.addAttribute("posts", posts);
         return "home";
     }
 
-    @PostMapping("/add/{id}/posts")
-    public Post savePost(@PathVariable(value = "id") Integer id, @RequestBody Post postRequest) {
+    @PostMapping("/addPost/{id}")
+    public String savePost(@ModelAttribute @PathVariable(value = "id") Integer id, @RequestBody Post postRequest, RedirectAttributes redirectAttributes) {
         Post post = userRepository.findById(id).map(user -> {
             postRequest.setUser(user);
             return postRepository.save(postRequest);
         }).orElseThrow();
-        return postRepository.save(post);
+        List<Post> allPost =postRepository.findAll();
+        redirectAttributes.addFlashAttribute("post", allPost);
+        redirectAttributes.addFlashAttribute("user", post.getUser());
+        return "redirect:/home/" + post.getUser().getId();
     }
 
 }
